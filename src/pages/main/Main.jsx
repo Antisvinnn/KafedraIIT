fd0264a1b90da1e0236328cc314fc0e6ae35c14e
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-05-07 02:41:28 +0300

    footer

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -93,6 +93,21 @@ const Main = () => {
 					</Parallax>
 				</div>
 			</div>
+			<footer className={style.footer}>
+				<div className={style.leftContent}>
+					<div className={style.leftContentText}>
+						©2021 Кафедра ИИТ. Все права защищены. <br />
+						Email: iit@bstu.by
+						<br />
+						Контактный номер: 8 (0132) 2-56-11
+					</div>
+				</div>
+				<div className={style.rightContent}>
+					<div className={style.rightContentText}>
+						Разработка и дизайн сайта - Точёный Виталий
+					</div>
+				</div>
+			</footer>
 		</div>
 	);
 };
