721cd8241ee14cac6633da49f585a02799546301
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-05-09 04:35:59 +0300

    redux scelet implemented

--- app/src/redux/reducers/index.js
+++ app/src/redux/reducers/index.js
@@ -1,1 +1,12 @@
-//import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+import authReducer from './auth';
+import usersReducer from './users';
+
+const appReducer = combineReducers({
+	auth: authReducer,
+	users: usersReducer,
+});
+
+export const rootReducer = (state, action) => {
+	return appReducer(state, action);
+};
