c216473065e4b2938594777cc501d16182d6828d
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-05-23 20:00:41 +0300

    redux fixed

--- app/src/redux/reducers/index.js
+++ app/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
 import publickReducer from './publick';
+import authReducer from '../reducers/auth';
 
 const appReducer = combineReducers({
 	user: userReducer,
 	publick: publickReducer,
+	auth: authReducer,
 });
 
 export const rootReducer = (state, action) => {
