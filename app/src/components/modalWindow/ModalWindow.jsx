d517cc74f4655d8336d0f9a5d56ceffaf5aad54e
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-06-02 02:16:08 +0300

    fixes on modal

--- app/src/components/modalWindow/ModalWindow.jsx
+++ app/src/components/modalWindow/ModalWindow.jsx
@@ -1,11 +1,53 @@
 import style from './style.module.scss';
-import { Input, Modal, Upload } from 'antd';
+import { Input, Modal, Upload, message, Button } from 'antd';
 import Form from 'antd/lib/form/Form';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import FormItem from 'antd/lib/form/FormItem';
 
 const ModalWindow = ({ visible = false, setVisible = () => {}, typeOfAction }) => {
+	const [state, setState] = useState({ loading: false });
 	const handleOk = () => {
 		setVisible(false);
 	};
+	const beforeUpload = (file) => {
+		const isJpgOrPng =
+			file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/jpg';
+		if (!isJpgOrPng) {
+			message.error('Допускаются только JPG/PNG файлы!');
+			return;
+		}
+		const isLt2M = file.size / 1024 / 1024 < 4;
+		if (!isLt2M) {
+			message.error('Изображение должно быть меньше чем 4MB!');
+			return;
+		}
+		file.status = 'done';
+		return false;
+	};
+	function getBase64(img, callback) {
+		const reader = new FileReader();
+		reader.addEventListener('load', () => callback(reader.result));
+		reader.readAsDataURL(img);
+	}
+	const handleChange = (info) => {
+		if (info.file.status === 'uploading') {
+			setState({ loading: true });
+			return;
+		}
+		if (info.file.status === 'done') {
+			getBase64(info.file.originFileObj, (imageUrl) =>
+				setState({
+					imageUrl,
+					loading: false,
+				})
+			);
+		}
+	};
+	const getValues = (values) => {
+		console.log(values);
+	};
+
 	return (
 		<Modal
 			centered
@@ -13,25 +55,47 @@ const ModalWindow = ({ visible = false, setVisible = () => {}, typeOfAction }) =
 			visible={visible}
 			onCancel={() => setVisible(false)}
 			closable={false}
+			footer={false}
 			onOk={handleOk}
 		>
-			<Form>
-				<Input className={style.input} placeholder='ФИО (Иванов Иван Иванович)' />
-				<Input className={style.input} placeholder='Описание (Кандидат наук, доцент)' />
-				<Input
-					className={style.input}
-					placeholder='Контакты (Моб.номер и прочие ссылки на контакты)'
-				/>
-				<Input className={style.input} placeholder='Логин' />
-				<Input className={style.input} placeholder='Пароль' />
-				<Upload
-					name='avatar'
-					listType='picture-card'
-					className='avatar-uploader'
-					showUploadList={false}
-				>
-					Загрузить фото
-				</Upload>
+			<Form onFinish={getValues}>
+				<FormItem className={style.formItem} name='name'>
+					<Input className={style.input} placeholder='ФИО (Иванов Иван Иванович)' />
+				</FormItem>
+				<FormItem className={style.formItem} name='description'>
+					<Input className={style.input} placeholder='Описание (Кандидат наук, доцент)' />
+				</FormItem>
+				<FormItem className={style.formItem} name='contacts'>
+					<Input
+						className={style.input}
+						placeholder='Контакты (Моб.номер и прочие ссылки на контакты)'
+					/>
+				</FormItem>
+				<FormItem className={style.formItem} name='login'>
+					<Input className={style.input} placeholder='Логин' />
+				</FormItem>
+				<FormItem className={style.formItem} name='password'>
+					<Input className={style.input} placeholder='Пароль' />
+				</FormItem>
+				<FormItem className={style.formItem} name='upload'>
+					<Upload
+						beforeUpload={beforeUpload}
+						maxCount={1}
+						name='avatar'
+						onChange={handleChange}
+						listType='picture-card'
+						className={style.uploader}
+						showUploadList={false}
+					>
+						Загрузить фото
+					</Upload>
+				</FormItem>
+				<Button htmlType='submit' className={style.button} type='primary' onClick={handleOk}>
+					ОК
+				</Button>
+				<Button onClick={() => setVisible(false)} className={style.button} type='default'>
+					Отменить
+				</Button>
 			</Form>
 		</Modal>
 	);
--- app/src/pages/teacherPage/TeacherPage.jsx
+++ app/src/pages/teacherPage/TeacherPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams, useHistory } from 'react-router-dom';
