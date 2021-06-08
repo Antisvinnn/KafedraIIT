4652fb19e82aa9ddaabfdfeddd8f066ea2e0c2fc
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-06-09 00:26:39 +0300

    posts fix but a little bit problem

--- app/src/redux/reducers/user.js
+++ app/src/redux/reducers/user.js
@@ -4,12 +4,15 @@ import {
 	WHO_AM_I_SUCCESS,
 	WHO_AM_I_FAILED,
 	CLEAR_AUTH_DATA,
-	POST_TEACHER_POSTS,
+	POST_TEACHER_POSTS_REQUEST,
+	POST_TEACHER_POSTS_SUCCESS,
+	POST_TEACHER_POSTS_FAILED,
 } from '../actionsTypes/user';
 
 const initialState = {
 	profile: {},
 	isProfileLoading: false,
+	isPostTeacherPostsLoading: false,
 	authData: null,
 };
 
@@ -31,9 +34,17 @@ const userReducer = (state = initialState, action) => {
 			message.info('authData was cleared!');
 			newState.authData = null;
 			return newState;
-		case POST_TEACHER_POSTS:
-			newState.isProfileLoading = false;
+		case POST_TEACHER_POSTS_REQUEST:
+			newState.isPostTeacherPostsLoading = true;
+			return newState;
+		case POST_TEACHER_POSTS_SUCCESS:
+			newState.isPostTeacherPostsLoading = false;
 			newState.profile.posts = action.payload;
+			message.success('Запись опубликована');
+			return newState;
+		case POST_TEACHER_POSTS_FAILED:
+			newState.isPostTeacherPostsLoading = false;
+			message.error('Запись не будет опубликована!');
 			return newState;
 		default:
 			return state;
