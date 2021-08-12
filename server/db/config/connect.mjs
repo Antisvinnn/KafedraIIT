b042e2b24b757e0794d33ef513f7413cb0cb0990
Vitaly Tochenyy <Antisvinn@yandex.by>
2021-08-12 12:41:21 +0300

    media-queryes

--- server/db/config/connect.mjs
+++ server/db/config/connect.mjs
@@ -1,24 +1,24 @@
-import Sequelize from "sequelize";
-import Config from "./config.mjs";
-import dotenv from "dotenv";
+import Sequelize from 'sequelize';
+import Config from './config.mjs';
+import dotenv from 'dotenv';
 
 dotenv.config();
-const config = Config[process.env.NODE_ENV || "development"];
+const config = Config[process.env.NODE_ENV || 'development'];
 
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: config.host,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
+	config.database,
+	config.username,
+	process.env.POSTGRES_PASSWORD,
+	{
+		host: config.host,
+		dialect: 'postgres',
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+	}
 );
 
 export default sequelize;
--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
-import dotenv from "dotenv";
-import photo from "./constants/adminPhoto.json";
-import sequelize from "./db/config/connect.mjs";
-import { create } from "./services/UserService.mjs";
-import app from "./app.mjs";
-import logger from "./logs/pino.mjs";
+import dotenv from 'dotenv';
+import photo from './constants/adminPhoto.json';
+import sequelize from './db/config/connect.mjs';
+import { create } from './services/UserService.mjs';
+import app from './app.mjs';
+import logger from './logs/pino.mjs';
 
 dotenv.config();
 
-const PORT = process.env.NODE_ENV === "development" ? 33433 : process.env.PORT;
+const PORT = process.env.NODE_ENV === 'development' ? 33433 : process.env.PORT;
 const password = process.env.ADMIN_PASSWORD;
 
 const server = app.listen(PORT, (err) => {
-  if (err) logger.error(err);
-  else logger.info(`Server runing on PORT: ${PORT}`);
+	if (err) logger.error(err);
+	else logger.info(`Server runing on PORT: ${PORT}`);
 });
 server.setTimeout(3000000);
 
 try {
-  await sequelize.authenticate();
-  logger.info("Connection has been established successfully");
-  create({
-    name: "Admin",
-    photo: photo.photo,
-    description: "admin",
-    login: "admin",
-    role: "admin",
-    password,
-  });
+	await sequelize.authenticate();
+	logger.info('Connection has been established successfully');
+	create({
+		name: 'Admin',
+		photo: photo.photo,
+		description: 'admin',
+		login: 'admin',
+		role: 'admin',
+		password,
+	});
 } catch (error) {
-  logger.error("Unable to connect to the database:", error);
+	logger.error('Unable to connect to the database:', error);
 }
